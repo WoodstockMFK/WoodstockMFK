84fece6a7b77bcba95fca60f9b4172957acb464a
Lars Overvad Rasmussen <LSR@teknologisk.dk>
2026-04-25 17:50:05 +0200

    gemmer

--- images/Fotogalleri/Fly/rename.bat
+++ images/Fotogalleri/Fly/rename.bat
@@ -4,9 +4,16 @@ setlocal enabledelayedexpansion
 set count=1
 
 for %%F in (*.jpg) do (
+    ren "%%F" "__tmp__!count!.jpg"
+    set /a count+=1
+)
+
+set count=1
+
+for %%F in (__tmp__*.jpg) do (
     ren "%%F" "foto!count!.jpg"
     set /a count+=1
 )
 
-echo Færdig.
+echo Done!
 pause
